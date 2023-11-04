1f786f3405442697978e7b3705bf2d1c984f147e
Heunsig <heun3344@gmail.com>
2023-11-04 19:29:38 +0900

    set redis

--- index.ts
+++ index.ts
@@ -1,10 +1,18 @@
+import { createClient } from 'redis';
 import express from 'express';
 
 const PORT = 8000;
 
+const client = await createClient()
+  .on('error', err => {
+    console.log(err);
+  })
+  .connect();
+
 const app = express();
 
 app.get('/', (req, res) => {
+  console.log('Hello World');
   res.send('Hello From Express with TS');
 });
 
