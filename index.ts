9cbd6b7c8c0f466f8668672d65768dc602c6c11c
Heunsig <heun3344@gmail.com>
2023-11-04 17:10:17 +0900

    set up scripts

--- index.ts
+++ index.ts
@@ -5,8 +5,9 @@ const PORT = 8000;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello From Express');
+  res.send('Hello From Express with TS');
 });
+
 
 app.listen(PORT, () => {
   console.log(`now listening on port ${PORT}`);
