a2bbe2cad90f711ca5678deabe4fe8c48a0e4866
Heunsig <heun3344@gmail.com>
2023-11-04 19:43:02 +0900

    set ejs

--- index.ts
+++ index.ts
@@ -11,11 +11,22 @@ const client = await createClient()
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  console.log('Hello World');
-  res.send('Hello From Express with TS');
+  // console.log('Referrer', req.headers.referer);
+  res.render('index');
 });
 
+app.get('/posts/:postId', (req, res) => {
+  // console.log('id', req.params.postId);
+  res.render('post');
+});
+
+app.get('/hit', (req, res) => {
+  console.log('Hello World');
+  // res.render('index');
+});
 
 app.listen(PORT, () => {
   console.log(`now listening on port ${PORT}`);
